c41973285c99e4e3dd38c1037a7dc345209c79ea
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-10-04 18:40:43 -0500

    hw draft5

--- public/app.js
+++ public/app.js
@@ -1,23 +1,22 @@
-$.getJSON('/articles', function(data) {
+$.getJSON('/articles', function (data) {
 
-    for (var i = 0; i<data.length; i++){
-  
-      // PAGE DISPLAY //
-  
-      $('#articles').append('<p data-id="' + data[i]._id + '">'+ data[i].title + '<br />'+ data[i].link + '</p>');
-    }
-  });
-  
-  $(document).on('click', 'a', function(){
-  
-    $('#notes').empty();
-  
-    var thisId = $(this).attr('data-id');
-  
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId,
-    })
-  
-        
-  });
+  for (var i = 0; i < data.length; i++) {
+
+    // PAGE DISPLAY //
+
+    $('#articles').append('<p data-id="' + data[i]._id + '">' + data[i].title + '<br />' + data[i].link + '</p>');
+  }
+});
+
+$(document).on('click', 'a', function () {
+
+  $('#notes').empty();
+
+  var thisId = $(this).attr('data-id');
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId,
+  })
+
+});
--- server.js
+++ server.js
@@ -5,17 +5,20 @@ var app = express();
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-var request = require('request'); 
+var request = require('request');
 var cheerio = require('cheerio');
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
+// PUBLIC STATIC DIRECTORY //
+
+app.use(express.static('public'));
 
 // PORT 3000 //
 
-app.listen(3000, function() {
-  console.log('App running on port 3000!');
+app.listen(3000, function () {
+    console.log('App running on port 3000!');
 });
