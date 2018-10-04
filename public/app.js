a197e2fe8cd5c9002d73d8604291ddadc6f4ac14
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-10-04 18:53:29 -0500

    hw draft6

--- public/app.js
+++ public/app.js
@@ -1,22 +1,28 @@
+// ARTICLE to JSON //
 $.getJSON('/articles', function (data) {
 
   for (var i = 0; i < data.length; i++) {
 
     // PAGE DISPLAY //
-
     $('#articles').append('<p data-id="' + data[i]._id + '">' + data[i].title + '<br />' + data[i].link + '</p>');
   }
 });
 
 $(document).on('click', 'a', function () {
-
+    // EMPTY NOTE //
   $('#notes').empty();
 
   var thisId = $(this).attr('data-id');
 
+    // AJAX CALL for ARTICLE //
   $.ajax({
     method: "GET",
     url: "/articles/" + thisId,
   })
+  .done(function( data ) {
+    console.log(data);
+
+  });
+
 
 });
