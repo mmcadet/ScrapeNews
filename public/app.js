eb1b67c7862e99495675ef040d40ea8a1c54fc20
mmcadet@gmail.com <34467088+mmcadet@users.noreply.github.com>
2018-10-04 20:31:55 -0500

    hwdraft11

--- public/app.js
+++ public/app.js
@@ -39,8 +39,19 @@ $(document).on('click', '#savenote', function(){
   // ARTICLE ID from SUBMIT BUTTON //
 
   var thisId = $(this).attr('data-id');
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $('#titleinput').val(),
+      body: $('#bodyinput').val()
+    }
+  }).done(function( data ) {
+    console.log(data);
+
+    $('#notes').empty();
+  });
   
-  
-      console.log(data);
+
 
 });
